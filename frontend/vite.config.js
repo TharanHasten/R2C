2cef291799cd43018bda8662f0e799aea9c3e5f4
TharanHasten <tharan.a2023it@sece.ac.in>
2025-06-02 23:15:17 +0530

    backedurl updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://r2c-e8f5.onrender.com',
         changeOrigin: true,
       },
     },
